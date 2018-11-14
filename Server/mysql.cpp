873d305e2b5fccb785c358a1c6cbe088d03ee1ea
Ronghui Yu <hui@elion.tech>
2018-11-14 16:58:08 +0800

    create server

--- Server/mysql.cpp
+++ Server/mysql.cpp
@@ -6,12 +6,12 @@ MySql::MySql()
 }
 void MySql::initSql()
 {
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL", "ChatConnection");
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("127.0.0.1");
     db.setPort(3306);
     db.setUserName("root");
     db.setPassword("12345678");
-//    db.setDatabaseName("ChatUser");
+    db.setDatabaseName("ChatUser");
     if(db.open()){
         qDebug()<<"Database opened successfully.";
         createTable();
@@ -25,12 +25,14 @@ void MySql::initSql()
 }
 void MySql::createDatabase()
 {
-    query = new QSqlQuery;
-    query->
+    QSqlQuery query;
+    query.exec("create database if not exists chat");
 }
 void MySql::createTable()
 {
-
+    QSqlQuery query;
+    query.exec("create table if not exists users(username VARCHAR(30) primary key unique not null, password VARCHAR(30) not null");
+    query.exec("insert ignore into users value('root', '123456')");
 }
 bool MySql::login(QString username, QString password)
 {
