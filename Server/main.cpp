6f5bd0e30fd0d2c8438b7337ffd3ac5000556591
Ronghui Yu <hui@elion.tech>
2018-11-19 01:46:31 +0800

    server

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,11 +1,16 @@
 #include <QCoreApplication>
 #include <QTcpServer>
 #include <QDir>
+#include "server.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 #ifdef _DEBUG
-    auto dir = QDir
+    auto dir = QDir(QCoreApplication::applicationDirPath());
+#else
+    auro dir = QDir(QDir::currentPath());
+#endif
+    Server server(&a);
     return a.exec();
 }
