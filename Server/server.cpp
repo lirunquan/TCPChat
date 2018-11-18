118277ebf2411cb664123d487d4bb9c513b35d07
Ronghui Yu <hui@elion.tech>
2018-11-18 22:56:06 +0800

    server

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,6 +1,43 @@
 #include "server.h"
+#include "messagequeue.h"
+#include "user.h"
+
+#include <QByteArray>
+#include <QTime>
+int cur;
+
+struct user_data
+{
+    User* u[M];
+    int size;
+}*User_data;
+
+MessageQueue* Message;
+bool isFirst = false;
+int mode[M];
+enum{
+    AcceptLogin = 0,
+    Chat
+};
+int indexOf;
 
 Server::Server(QObject *parent) : QObject(parent)
+{
+    User_data = new user_data();
+    for(int i=0;i<M;i++){
+        User_data->u[i] = NULL;
+        mode[i] = Chat;
+    }
+    indexOf = 0;
+    User_data->size = 0;
+    cur = 0;
+    Message = NULL;
+    isFirst = true;
+    userLoaded();
+    userStateUpdate();
+
+}
+void Server::userLoaded()
 {
 
 }
--- Server/server.h
+++ Server/server.h
@@ -8,15 +8,26 @@
 #include <QTimer>
 #include <QtNetwork>
 
+const int M = 20;
+
 class Server : public QObject
 {
     Q_OBJECT
 public:
     explicit Server(QObject *parent = nullptr);
-
+    ~Server();
+    void userLoaded();
+    void handleMessage(QString m_name, QTcpSocket* socket);
+    void userStateUpdate();
+    void saveToFile();
 signals:
 
 public slots:
+
+private:
+    QTcpServer* tcpServer;
+    QTcpSocket* tcpSocket;
+    QTimer timer;
 };
 
 #endif // SERVER_H
