0b0888b3d3893788243b39dfeeac2d46f6481149
Ronghui Yu <hui@elion.tech>
2018-11-20 00:00:39 +0800

    fix mode[]

--- Server/server.cpp
+++ Server/server.cpp
@@ -32,7 +32,7 @@ void Server::init()
     User_data = new user_data();
     for(int i=0;i<M;i++){
         User_data->u[i] = NULL;
-        mode[i] = Chat;
+        mode[i] = AcceptLogin;
     }
     indexOf = 0;
     User_data->size = 0;
